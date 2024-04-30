a0c0a1cd10ab13561b698506aaa633653666d88e
Mariusz Januszek <mariusz.januszek95@gmail.com>
2024-04-30 15:14:43 +0200

    Comments

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -1,19 +1,23 @@
 const { expect, browser, $ } = require('@wdio/globals');
 
-describe('My Login application', () => {
-  it('should create pastebin post', async () => {
-    await browser.url(`https://pastebin.com/`);
+describe('Create a pastebin post', () => {
+  it('should create a new paste with specific attributes', async () => {
+    // Open the pastebin website
+    await browser.url('https://pastebin.com/');
 
+    // Enter the code
     await $('#postform-text').setValue('Hello from WebDriver');
+
+    // Enter the paste name
     await $('#postform-name').setValue('helloweb');
 
-    await $('#select2-postform-expiration-container').click()
-    await $('.select2-results__option[id*="10M"]').click()
+    // Click on the paste expiration dropdown
+    await $('#select2-postform-expiration-container').click();
 
+    // Select the option for 10 Minutes expiration
+    await $('.select2-results__option[id*="10M"]').click();
 
-    await $('.form-btn-container button').click()
-
-    await browser.pause(10000);
-
+    // Click the button to create the paste
+    await $('.form-btn-container button').click();
   });
 });
