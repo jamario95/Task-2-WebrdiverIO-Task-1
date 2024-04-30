1fd15bfddf78ed8d72a0894a0157aab21d764faa
Mariusz Januszek <mariusz.januszek95@gmail.com>
2024-04-30 15:10:51 +0200

    Working script

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -1,16 +1,19 @@
-const { expect, browser, $ } = require('@wdio/globals')
+const { expect, browser, $ } = require('@wdio/globals');
 
 describe('My Login application', () => {
-    it('should login with valid credentials', async () => {
-        await browser.url(`https://the-internet.herokuapp.com/login`)
+  it('should create pastebin post', async () => {
+    await browser.url(`https://pastebin.com/`);
 
-        await $('#username').setValue('tomsmith')
-        await $('#password').setValue('SuperSecretPassword!')
-        await $('button[type="submit"]').click()
+    await $('#postform-text').setValue('Hello from WebDriver');
+    await $('#postform-name').setValue('helloweb');
 
-        await expect($('#flash')).toBeExisting()
-        await expect($('#flash')).toHaveTextContaining(
-            'You logged into a secure area!')
-    })
-})
+    await $('#select2-postform-expiration-container').click()
+    await $('.select2-results__option[id*="10M"]').click()
 
+
+    await $('.form-btn-container button').click()
+
+    await browser.pause(10000);
+
+  });
+});
